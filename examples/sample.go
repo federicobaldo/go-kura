a05591aabf58c124e5a5dc196181b2298fc3b905
Federico Baldo <federico.baldo@eurotech.com>
2016-06-30 17:57:39 +0200

    - initial commit

--- examples/sample.go
+++ examples/sample.go
@@ -1,15 +1,3 @@
-# go-kura
-Some utilities to parse Eclipse Kura payloads in Go.
-
-## Kura Datatype
-The kuradatatypes package contains the protobuf generated Go code for the Kura payload
-as defined in the Kura repository:
-
-https://github.com/eclipse/kura/blob/develop/kura/org.eclipse.kura.core.cloud/src/main/protobuf/kurapayload.proto
-
-In order to use it you can import it in your Go program using:
-
-```go
 package main
 
 import (
@@ -36,4 +24,3 @@ func main(){
 
     fmt.Printf("Here my Kura payload: %s\n", message)
 }
-```
